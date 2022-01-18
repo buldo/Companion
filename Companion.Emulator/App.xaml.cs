c9b65ce781946a87afab1263b2822e309a821f82
Roman Buldygin <froooks@gmail.com>
2022-01-18 11:48:10 +0300

    Init app

---
+++ Companion.Emulator/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Companion.Emulator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Companion.Emulator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Companion.Emulator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
