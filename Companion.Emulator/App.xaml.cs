03d83aa02660cefd5f5a3725e4f4d1261e066734
Roman Buldygin <froooks@gmail.com>
2022-01-18 15:21:15 +0300

    Recreate

--- Companion.Emulator/App.xaml.cs
+++ Companion.Emulator/App.xaml.cs
@@ -1,11 +1,17 @@
-﻿namespace Companion.Emulator;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
-public partial class App : Application
+namespace Companion.Emulator
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new MainPage();
-	}
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
 }
